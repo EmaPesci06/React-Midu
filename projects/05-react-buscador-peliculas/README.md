00519a1b0a58877334f4f8d0d9931fade506691e
Emanuel Pesci <emanuelpesci@gmail.com>
2024-01-11 15:49:41 -0300

    05-bucador-peliculas

---
+++ projects/05-react-buscador-peliculas/README.md
@@ -0,0 +1,25 @@
+## Enunciado
+
+Crea una aplicacion para buscar peliculas
+
+API a usar:
+
+-https://www.omdbapi.com/
+
+- API_KEY: 19573c3a
+
+Requirimentos:
+
+- Necesita mostrar un input para buscar la pelicula y un boton para buscar.
+
+- Que el formulario funcione
+
+- Listar las peliculas encontradas y se muestra el titulo, año y poster.
+
+- Haz que las peliculas se muestren en un grid responsive
+
+Primera iteraccion:
+
+- Evitar que se haga la misma busqueda dos veces seguidas
+- Haz que la busqueda se haga automaticamente al escribir
+- Evita que se haga la busqueda continuamente al escribir (debounce)
---
+++ projects/05-react-buscador-peliculas/src/App.jsx
@@ -0,0 +1,60 @@
+import { useCallback, useState } from "react";
+import "./App.css";
+import { Movies } from "./components/Movies";
+import { useMovies } from "./hooks/useMovies";
+import { useSearch } from "./hooks/useSearch";
+import debounce from "just-debounce-it";
+
+function App() {
+  const [sort, setSort] = useState(false);
+
+  const { search, updateSearch, error } = useSearch();
+  const { movies, getMovies, loading } = useMovies({ search, sort });
+
+  const debounceGetMovies = useCallback(
+    debounce((search) => {
+      console.log("search", search);
+      getMovies({ search });
+    }, 300),
+    [getMovies]
+  );
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    getMovies({ search });
+  };
+
+  const handleChange = () => {
+    const newSearch = event.target.value;
+    updateSearch(newSearch);
+    debounceGetMovies(newSearch);
+  };
+
+  const handleSort = () => {
+    setSort(!sort);
+  };
+
+  return (
+    <div className="page">
+      <header>
+        <h1>Buscador de peliculas</h1>
+        <form className="form" onSubmit={handleSubmit}>
+          <input
+            onChange={handleChange}
+            value={search}
+            placeholder="Avengers, Star Wars, The Matrix...."
+          />
+          <input type="checkbox" onChange={handleSort} checked={sort} />
+          <button type="submit">Buscar</button>
+        </form>
+        {error && <p style={{ color: "red" }}>{error}</p>}
+      </header>
+
+      <main>
+        {loading ? <p>Cargando......</p> : <Movies movies={movies} />}{" "}
+      </main>
+    </div>
+  );
+}
+
+export default App;
